0599d4b68974439f4e80b5d1d58e9b878d11ce08
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-06 18:21:47 +0530

    Testimonials

---
+++ react-project-7/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Testamonails from './components/Testamonails'
+
+const App = () => {
+  return (
+    <div>
+      <Testamonails />
+    </div>
+  )
+}
+
+export default App
