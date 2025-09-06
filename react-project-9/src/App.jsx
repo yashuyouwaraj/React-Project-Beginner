162e2ec246224a1b33f33e1827b72905a559cea5
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-06 20:57:49 +0530

    Form Validation

---
+++ react-project-9/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormValidation from './components/FormValidation'
+
+const App = () => {
+  return (
+    <div>
+      <FormValidation />
+    </div>
+  )
+}
+
+export default App
