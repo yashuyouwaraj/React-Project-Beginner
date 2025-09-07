d6e4511d70af9b28fd15a6584a65ed27a02444dd
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-07 17:30:43 +0530

    Refactoring Products.jsx

--- react-project-10/src/components/Card.jsx
+++ react-project-10/src/components/Card.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { AiFillStar } from "react-icons/ai";
+import { BsFillBagHeartFill } from "react-icons/bs";
 
 const card = () => {
   return (
-    <div>card</div>
-  )
-}
+    <section className="card">
+      <img
+        src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+        className="card-img"
+        alt="Shoe"
+      />
+      <div className="card-details">
+        <h3 className="card-title">Shoe</h3>
+        <section className="card-reviews">
+          <AiFillStar className="rating-star" />{" "}
+          <AiFillStar className="rating-star" />{" "}
+          <AiFillStar className="rating-star" />{" "}
+          <AiFillStar className="rating-star" />
+          <span className="total-reviews">4</span>
+        </section>
+        <section className="card-price">
+          <div className="price">
+            <del>$300</del> $200
+          </div>
+          <div className="bag">
+            <BsFillBagHeartFill className="bag-icon" />
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+};
 
-export default card
+export default card;
